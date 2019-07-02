69f44f582a5d5fb4dada5ac1bf82d702ac8b9e81
sudharsangs <g.s.sudharsan99@gmail.com>
2019-07-02 19:02:00 +0530

    Adding all backend files

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,6 @@ const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`);
-});
-
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true,useCreateIndex: true } );
@@ -23,3 +19,13 @@ connection.once('open', () => {
 	console.log("MongoDB database connection established");
 });
 
+const exercisesRouter  = require('./routes/exercises');
+const usersRouter  = require('./routes/users');
+
+app.use('/exercises',exercisesRouter);
+app.use('/users',usersRouter);
+
+
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
